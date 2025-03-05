04254786017ab51431552cee6708d24422d3c9dd
RUTHVIKA-05 <2210030284@klh.edu.in>
2025-03-05 10:11:21 +0530

    Create operators.js

---
+++ javascript/operators.js
@@ -0,0 +1,9 @@
+var a=10
+var b=15
+document.write("Addition ",a+b);
+document.write("<br> Substraction ",a-b);
+document.write("<br> Mutliplication ",a*b);
+document.write("<br> Division ",a/b);
+document.write("<br> Modulus ",a%b);
+document.write("<br> Increment output ",a++);
+document.write("<br> Increment output ",a--);
