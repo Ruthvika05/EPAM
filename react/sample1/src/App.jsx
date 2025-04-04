5fe6c4fac08791542ad35bf185aa115cd9d6c80b
RUTHVIKA-05 <2210030284@klh.edu.in>
2025-04-04 13:48:34 +0530

    react

---
+++ react/sample1/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <img src="/klh.png" alt="KL University Logo" className="logo" />
+      <h1 className="university-name">KL University</h1>
+    </div>
+  );
+}
+
+export default App;
